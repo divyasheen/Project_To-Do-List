64331afacf7d7be07b9c764e05f5cd9e43e4620c
Judith Bohmann <judith.bohmann@dci-student.org>
2025-02-12 14:30:39 +0100

    edit form in Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Add from './components/Add'
 
 function App() {
 
   return (
     <>
-      
+    <Add />
+     
     </>
   )
 }
--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
 function Add() {
+  const [data, setData] = useState({ heading: "", toDo: "" });
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+ const handleSubmit = (e) => {
+    e-preventDefault();
+
+  }
+
+  console.log(data);
+
   return (
     <>
       <div>Add</div>
+      <form onSubmit = {handleSubmit}>
+        <label>
+          Heading
+          <input
+            type="text"
+            name="heading"
+            value={data.heading}
+            placeholder="Type your heading here ... "
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          To-Do
+          <textarea
+            name="toDo"
+            value={data.toDo}
+            placeholder="Type your to-do here ..."
+            onChange={handleChange}
+          ></textarea>
+        </label>
+        <button>Save</button>
+      </form>
     </>
   );
 }
