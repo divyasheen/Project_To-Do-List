6de97e9cd44d5505c1fa93128f62467fbf01012d
Judith Bohmann <judith.bohmann@dci-student.org>
2025-02-12 14:40:36 +0100

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
-import Add from './components/Add'
+import "./App.css";
+import Add from "./components/Add";
+import { Routes, Route } from "react-router-dom";
+import Display from "./components/Display";
 
 function App() {
-
   return (
     <>
-    <Add />
-     
+    <Routes>
+      <Route index element = {<Display />}/>
+      <Route path = "/" element = {<Display />}/>
+      <Route path = "/add" element = {<Add />}/>
+    </Routes>
+    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
