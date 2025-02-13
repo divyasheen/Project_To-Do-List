1d9bdb7c8e2661dce63ea539831ff4a6ca642b0b
Judith Bohmann <judith.bohmann@dci-student.org>
2025-02-13 09:32:58 +0100

    You-did-bowman (#2)
    * edit Add
    
    * edit form in Add
    
    * add routes
    
    * minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import './App.css'
+import "./App.css";
+import Add from "./components/Add";
+import { Routes, Route, NavLink } from "react-router-dom";
+import Display from "./components/Display";
 import InspoPhrase from './components/InspoPhrase'
 
-function App() {
 
+function App() {
   return (
     <>
+    <Routes>
+      <Route index element = {<Display />}/>
+      <Route path = "/" element = {<Display />}/>
+      <Route path = "/add" element = {<Add />}/>
+    </Routes>
+
+    <NavLink to = "/add">Add</NavLink>
       <InspoPhrase/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
