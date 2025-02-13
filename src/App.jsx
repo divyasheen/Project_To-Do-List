7b84c1ce557edfab1bd14ad95715933aa033deb3
Divya Mariya Raphy <divya.raphy@dci-student.org>
2025-02-13 09:44:02 +0100

    display and filter (#3)
    Co-authored-by: Judith Bohmann <judith.bohmann@dci-student.org>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import "./App.css";
+import './App.css'
+import Display from './components/Display'
+import Category from './components/Category'
 import Add from "./components/Add";
 import { Routes, Route, NavLink } from "react-router-dom";
-import Display from "./components/Display";
 import InspoPhrase from './components/InspoPhrase'
+
 
 
 function App() {
   return (
     <>
+      <Category/>
     <Routes>
       <Route index element = {<Display />}/>
       <Route path = "/" element = {<Display />}/>
