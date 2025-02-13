7b57641c0916039ab4dfee9d3396c3714bc9dd9a
Judith Bohmann <judith.bohmann@dci-student.org>
2025-02-13 14:39:44 +0100

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import './App.css'
-import Display from './components/Display'
-import Category from './components/Category'
+import "./App.css";
+import Display from "./components/Display";
+import Category from "./components/Category";
 import Add from "./components/Add";
 import { Routes, Route, NavLink } from "react-router-dom";
-import InspoPhrase from './components/InspoPhrase'
-
-
+import InspoPhrase from "./components/InspoPhrase";
 
 function App() {
   return (
     <>
-    <InspoPhrase/>
-      <Category/>
+      <InspoPhrase />
+      {/* This div should be in Display.jsx. There it should contain <Filter /> as well.
+      But don't want to interfer with Divya =D */}
+      <div className = "menu">
+      <button><NavLink to="/add">Add</NavLink> </button>
+        <Category />
+      </div>
 
-    <Routes>
-      <Route index element = {<Display />}/>
-      <Route path = "/" element = {<Display />}/>
-      <Route path = "/add" element = {<Add />}/>
-    </Routes>
-
-    <NavLink to = "/add">Add</NavLink>
-      
+      <Routes>
+        <Route index element={<Display />} />
+        <Route path="/" element={<Display />} />
+        <Route path="/add" element={<Add />} />
+      </Routes>
     </>
   );
 }
