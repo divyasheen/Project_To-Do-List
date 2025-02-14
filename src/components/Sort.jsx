1655a5870ef2ab9ed14d5e54fba58ffa1e117cbf
Nataliia <nataliia.lishchynska@dci-student.org>
2025-02-14 11:46:14 +0100

    comitting

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Sort({setSort}) {
   return (
     <select onChange={(e) => setSort(e.target.value)}>
-        <option value="sort">Sort:</option>
+        <option value="">Sort:</option>
       <option value="important">Important first</option>
       <option value="new">New first</option>
       <option value="latest">Latest first</option>
