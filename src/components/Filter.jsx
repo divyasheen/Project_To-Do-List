b3d4398332e8b60a9a8665e2e5b1642fd1980ef6
Judith Bohmann <judith.bohmann@dci-student.org>
2025-02-17 12:39:12 +0100

    add style to inspo filter index add and display

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -4,7 +4,7 @@ function Filter({ setFilter }) {
   return (
     <div className="selectFilter">
       <select onChange={(e) => setFilter(e.target.value)}>
-        <option value="">Filter:</option>
+        <option value="">Filter: </option>
         <option value="All">All</option>
         <option value="Completed">Completed</option>
         <option value="Pending">Pending</option>
