be374f6184d4f192dff474b43abfadd25b1403d2
Judith Bohmann <judith.bohmann@dci-student.org>
2025-02-18 10:30:48 +0100

    edit Not Found

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import "./NotFound.css";
+
 function NotFound() {
+const navigate = useNavigate()
+
   return (
     <>
-      <div className="todo-container">
+      <div className="not-found-container">
         <h1>Oops!</h1>
         <h3>404 - PAGE NOT FOUND</h3>
         <p>Sorry, the page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
-        <button>Go back to main</button>
+        <button className = "go-back" onClick={() => navigate("/")}>Go back to main</button>
      
       
     </div>
