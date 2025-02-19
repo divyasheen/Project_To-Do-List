6029d1421d7f5ef8b8d9186544f1bba8861761b6
Judith Bohmann <judith.bohmann@dci-student.org>
2025-02-19 11:50:36 +0100

    edit notfound

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -10,6 +10,7 @@ const navigate = useNavigate()
         <h1>Oops!</h1>
         <h3>404 - PAGE NOT FOUND</h3>
         <p>Sorry, the page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+        <p className="superhero">🦸🏻‍♀️🦸🏻‍♀️🦸🏻‍♀️🦸🏻‍♀️</p>
         <button className = "go-back" onClick={() => navigate("/")}>Go back to main</button>
      
       
