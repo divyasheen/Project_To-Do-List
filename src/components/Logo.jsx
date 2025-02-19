8ee0613ea5510cc63ef9e3aef1d8787c499aa3c3
Judith Bohmann <judith.bohmann@dci-student.org>
2025-02-19 10:22:53 +0100

    You did bowman (#35)
    * edit Add
    
    * edit form in Add
    
    * add routes
    
    * minor changes
    
    * add Context and ID logic
    
    * minor changes to the context and add after meeting about connecting add with display
    
    * add styles
    
    * styles
    
    * add comments
    
    * add style to Logo and InspoPhrase
    
    * add style to App and Display
    
    * add style to Add
    
    * solve mergin problems
    
    * solve merge problems which are infecting the functionality
    
    * add style to Add and tidy up styles
    
    * add style to inspo filter index add and display
    
    * edit css files
    
    * commit
    
    * solve style problems after merging
    
    * add NotFoun with some content
    
    * edit Not Found
    
    * edit style
    
    * minor changes in display
    
    * edit style everywhere
    
    * media on footer
    
    * add animation cat
    
    * fix button
    
    * add bg
    
    * solve go back problem
    
    * build
    
    * solve cate maybe
    
    * hopefully solve cat

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,11 @@
 import "./Logo.css";
+import cat from "../assets/cat.png";
 function Logo() {
   return (
     <>
     <div className="logo">
       <h1>To-Do</h1>
-      <img src="src/assets/cat.png" alt="cat" />
+      <img src={cat} alt="cat" />
     </div>
     
     </>
